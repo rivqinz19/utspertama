0a727b836af2047634af641a573ada626a1332ea
rivqinz19 <rifqibarcelona@gmail.com>
2023-05-14 19:11:24 +0700

    UTSPBO

--- pengguna/User.java
+++ pengguna/User.java
@@ -1,5 +1,24 @@
 package pengguna;
 
-public class User {
+public class User { 
+    private String username;
+    private String password;
+
+public void setUsername(String username) {
+    this.username = username ;
+}
+
+public String getUsername(){
+    return this.username ;
+}
+
+public void setPassword(String password){
+    this.password = password ;
+}
+
+public void getPassword(){
+    return this.password ;
+    
+}
 
 }
